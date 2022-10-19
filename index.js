f38a309d726bdb7bdb042191b518dfc2fcc3c0be
JoseMonroy <mnry7999@gmail.com>
2022-10-18 19:46:04 -0500

    Metodo children y section
    El metodo children permite crear string con formstos distintos, ya sea letras en negris, subrayyas, etc. Sections nos permite posicionar en un una sección del archivo, es decir a reglon seguido, siguiente pagina, etc

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
 const docx = require('docx');
 const fs = require('fs');
 
-const doc = new docx.Document({});
+const doc = new docx.Document({
+    sections: [{
+        properties: {},
+        children: [
+            new docx.Paragraph({
+                children: [
+                    new docx.TextRun("Hello World"),
+                    new docx.TextRun({
+                        text: "Foo Bar",
+                        bold: true,
+                    }),
+                ],
+            }),
+        ],
+    }, ],
+});
