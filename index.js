d29600f180ce7a42e315a33f38490b4f63d34091
JoseMonroy <mnry7999@gmail.com>
2022-10-19 19:09:10 -0500

    Creación de tabla básica
    Se usa una sección nueva para evitar que se omitan datos

--- index.js
+++ index.js
@@ -1,50 +1,76 @@
 const docx = require('docx');
 const fs = require('fs');
-const { Paragraph, ImageRun } = docx;
+const { Paragraph, ImageRun, TableRow, TableCell, Table, SectionType } = docx;
+
+const table = new Table({
+    rows: [
+        new TableRow({
+            children: [
+                new TableCell({
+                    children: [new Paragraph("Hello")],
+                    columnSpan: 2,
+                }),
+            ],
+        }),
+        new TableRow({
+            children: [
+                new TableCell({
+                    children: [new Paragraph("World")],
+                }),
+            ],
+        }),
+    ],
+});
 
 const doc = new docx.Document({
     sections: [{
-        properties: {},
-        children: [
-            new docx.Paragraph({
-                children: [
-                    new docx.TextRun("Hello World"),
-                    new docx.TextRun({
-                        text: "Foo Bar",
-                        bold: true,
-                    }),
-                ],
-            }),
-            new Paragraph("Metodo para crear texto sin estilos"),
-            new Paragraph("Inserción básica de imagen"),
-            new Paragraph({
-                children: [
-                    new ImageRun({
-                        data: fs.readFileSync("./images/index.png"),
-                        transformation: {
-                            width: 100,
-                            height: 100,
-                        },
-                    }),
-                    new Paragraph({
-                        children: [
-                            new ImageRun({
-                                data: fs.readFileSync("./images/pokemon.jpeg"),
-                                transformation: {
-                                    width: 100,
-                                    height: 100,
-                                    flip: {
-                                        horizontal: true,
+            children: [
+                new docx.Paragraph({
+                    children: [
+                        new docx.TextRun("Hello World"),
+                        new docx.TextRun({
+                            text: "Foo Bar",
+                            bold: true,
+                        }),
+                    ],
+                }),
+                new Paragraph("Metodo para crear texto sin estilos"),
+                new Paragraph("Inserción básica de imagen"),
+                new Paragraph({
+                    children: [
+                        new ImageRun({
+                            data: fs.readFileSync("./images/index.png"),
+                            transformation: {
+                                width: 100,
+                                height: 100,
+                            },
+                        }),
+                        new Paragraph({
+                            children: [
+                                new ImageRun({
+                                    data: fs.readFileSync("./images/pokemon.jpeg"),
+                                    transformation: {
+                                        width: 100,
+                                        height: 100,
+                                        flip: {
+                                            horizontal: true,
+                                        },
+                                        rotation: 225,
                                     },
-                                    rotation: 225,
-                                },
-                            }),
-                        ],
-                    }),
-                ],
-            }),
-        ]
-    }, ],
+                                }),
+                            ],
+                        }),
+                    ],
+                }),
+            ],
+        },
+        {
+            children: [
+                new Paragraph("Inserción de tabla"),
+                table,
+            ],
+        },
+    ]
 });
 
 docx.Packer.toBuffer(doc).then((buffer) => {
